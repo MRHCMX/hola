c5c61cd5889a7ae8ba5cdb36c9977f6dff9cdc99
MarHerna <mrhcmex@yahoo.com.mx>
2022-04-06 17:55:01 -0500

    First commit

---
+++ src/main/java/com/mrhc/saludos/App.java
@@ -0,0 +1,18 @@
+package com.mrhc.saludos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void hola() {
+    	System.out.print("Hello World!");
+    	
+    }
+}
